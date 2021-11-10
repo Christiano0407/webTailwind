047f9a28b165e17cf829ab9dcb88a9f3d5662ee9
Christiano0407 <placebo0704@gmail.com>
2021-11-09 21:23:04 -0600

    tailwind 06

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,15 @@ module.exports = {
       // => @media (min-width: 1280px) { ... }
       "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
+    fontFamily: {
+      sans: ["Lato", `sans-serif`],
     },
   },
   variants: {
