78ea1f124734c7c2e39bc125e2e5c80fb0714bc2
Christiano0407 <placebo0704@gmail.com>
2021-11-09 20:41:06 -0600

    webTailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,25 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    minWidth: {
+      0: "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+    },
+    screen: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   variants: {
     extend: {},
