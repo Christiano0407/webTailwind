ea800d7a414cc7dc6df6280277ecdc9186cdd362
Christiano0407 <placebo0704@gmail.com>
2021-11-15 12:24:37 -0600

    build:CSS-Pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [`./public/**/*.html`],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "wave-pattern": `url(../img/wave-white.png)`,
+      }),
+    },
     minWidth: {
       0: "0",
       "1/4": "25%",
